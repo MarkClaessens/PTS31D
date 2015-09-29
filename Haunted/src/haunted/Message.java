44e8c2c7602b9dc08652312ee3bf791056f39f38
Kevin <kevin_stoelers@hotmail.com>
2015-09-29 10:58:08 +0200

    Message javadocs
    Message javadocs aangevuld

--- Haunted/src/haunted/Message.java
+++ Haunted/src/haunted/Message.java
@@ -7,18 +7,34 @@ public class Message {
 	private Time timeStamp;
 	private String text;
 
+        /**
+         * Returns the timestamp when the message was created
+         * @return timestamp
+         */
 	public Time getTimeStamp() {
 		return this.timeStamp;
 	}
 
+        /**
+         * Sets the timestamp when the message was created
+         * @param timeStamp 
+         */
 	public void setTimeStamp(Time timeStamp) {
 		this.timeStamp = timeStamp;
 	}
 
+        /**
+         * Return the text contained in the message
+         * @return 
+         */
 	public String getText() {
 		return this.text;
 	}
 
+        /**
+         * Sets the text contained in the message
+         * @param text 
+         */
 	public void setText(String text) {
 		this.text = text;
 	}
