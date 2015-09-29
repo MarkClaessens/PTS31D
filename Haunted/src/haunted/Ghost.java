7fe23f159e931b1f904d3d65ac91280f77e26429
cyrilbrugman <cyril.brugman@gmail.com>
2015-09-29 11:23:04 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Haunted/src/haunted/Human.java

--- Haunted/src/haunted/Ghost.java
+++ Haunted/src/haunted/Ghost.java
@@ -2,34 +2,67 @@ package haunted;
 
 public class Ghost extends Character {
 
-	private boolean vulnerable;
+    private boolean vulnerable;
+    private boolean isGhost;
 
-	public boolean isVulnerable() {
-		return this.vulnerable;
-	}
+    /**
+     * gives the state of vulnerability
+     *
+     * @return vulnerability
+     */
+    public boolean isVulnerable() {
+        return this.vulnerable;
+    }
 
-	public void setVulnerable(boolean vulnerable) {
-		this.vulnerable = vulnerable;
-	}
+    /**
+     * this sets the vulnerability of the ghost object
+     *
+     * @param vulnerable
+     */
+    public void setVulnerable(boolean vulnerable) {
+        this.vulnerable = vulnerable;
+    }
 
-	public void possess() {
-		// TODO - implement Ghost.possess
-		throw new UnsupportedOperationException();
-	}
+    /**
+     * This possesses a human, this ghost becomes the human, the previous human
+     * becomes a ghost.
+     */
+    public void possess() {
+        // TODO - implement Ghost.possess
+        // TODO - posses the human, works with Game.
+        throw new UnsupportedOperationException();
+    }
 
-	public void becomeAWall() {
-		// TODO - implement Ghost.becomeAWall
-		throw new UnsupportedOperationException();
-	}
+    /**
+     * Changes the appearance of the ghost, it becomes a wall or a ghost.
+     */
+    public void changeAppearance() {
+        // TODO - implement Ghost.changeApperance
+        if (this.isGhost) {
+            this.vulnerable = false;
+            this.setSprite("Wall");
+            this.isGhost = false;
+        } else {
+            this.vulnerable = true;
+            this.setSprite("Ghost");
+            this.isGhost = true;
+        }
+    }
 
-	public void vanish() {
-		// TODO - implement Ghost.vanish
-		throw new UnsupportedOperationException();
-	}
+    /**
+     * Removes the ghost when hit by the flashlight
+     */
+    public void vanish() {
+        // TODO - implement Ghost.vanish
+        throw new UnsupportedOperationException();
+    }
 
-	public Ghost() {
-		// TODO - implement Ghost.Ghost
-		throw new UnsupportedOperationException();
-	}
+    /**
+     * Constructor for ghost
+     */
+    public Ghost() {
+        // TODO - implement Ghost.Ghost
+        throw new UnsupportedOperationException();
+    }
 
 }
