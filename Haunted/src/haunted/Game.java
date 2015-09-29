1c3070647d51c6c6f112d53a420c182b27b99e05
MarkClaessens <mark-claessens@hotmail.com>
2015-09-29 11:34:29 +0200

    game javadoc

--- Haunted/src/haunted/Game.java
+++ Haunted/src/haunted/Game.java
@@ -9,41 +9,64 @@ public class Game {
 	private int currentRound;
 	private int currentFloor;
 	private Timer tickTimer;
-
+/**
+ * gives amount of floors the game contains
+ * @return 
+ */
 	public int getFloorAmount() {
 		return this.floorAmount;
 	}
-
+/**
+ * sets amount of floors the game will contain
+ */
 	public void setFloorAmount(int floorAmount) {
 		this.floorAmount = floorAmount;
 	}
-
+/**
+ * gives current round number
+ * @return 
+ */
 	public int getCurrentRound() {
 		return this.currentRound;
 	}
-
+/**
+ * sets current round number
+ * @param currentRound 
+ */
 	public void setCurrentRound(int currentRound) {
 		this.currentRound = currentRound;
 	}
-
+/**
+ * gives current floor number of the game
+ * @return 
+ */
 	public int getCurrentFloor() {
 		return this.currentFloor;
 	}
-
+/**
+ * sets current floor number of the game
+ * @param currentFloor 
+ */
 	public void setCurrentFloor(int currentFloor) {
 		this.currentFloor = currentFloor;
 	}
-
+/**
+ * gives the tick timer
+ * @return 
+ */
 	public Timer getTickTimer() {
 		return this.tickTimer;
 	}
-
+/**
+ * sets the tick timer
+ * @param tickTimer 
+ */
 	public void setTickTimer(Timer tickTimer) {
 		this.tickTimer = tickTimer;
 	}
 
 	/**
-	 * 
+	 * creates the game
 	 * @param players
 	 * @param floors
 	 */
@@ -53,34 +76,44 @@ public class Game {
 	}
 
 	/**
-	 * 
+	 * sets the game to the next level
 	 * @param floor
 	 */
 	public void nextLevel(int floor) {
 		// TODO - implement Game.nextLevel
 		throw new UnsupportedOperationException();
 	}
-
+/**
+ * starts the next round
+ */
 	public void startRound() {
 		// TODO - implement Game.startRound
 		throw new UnsupportedOperationException();
 	}
-
+/**
+ * ends the current round
+ */
 	public void endRound() {
 		// TODO - implement Game.endRound
 		throw new UnsupportedOperationException();
 	}
-
+/**
+ * ends the game
+ */
 	public void endGame() {
 		// TODO - implement Game.endGame
 		throw new UnsupportedOperationException();
 	}
-
+/**
+ * player leaves the game
+ */
 	public void leaveGame() {
 		// TODO - implement Game.leaveGame
 		throw new UnsupportedOperationException();
 	}
-
+/**
+ * everything that needs to be checked every tick from the timer
+ */
 	public void tick() {
 		// TODO - implement Game.tick
 		throw new UnsupportedOperationException();
