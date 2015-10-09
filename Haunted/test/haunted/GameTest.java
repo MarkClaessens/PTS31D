78af3c5cdd347ac1819bbf5676a264b345ae257d
Mike Evers <mike_evers1997@hotmail.com>
2015-10-09 11:56:51 +0200

    Finished Game Unittest (don't know how to test Tick() )

--- Haunted/test/haunted/GameTest.java
+++ Haunted/test/haunted/GameTest.java
@@ -7,6 +7,7 @@ package haunted;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
 import junit.framework.Assert;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -89,10 +90,16 @@ public class GameTest {
      * Increases the currentRound with 1, generates a new level object and calls the startRound method.
      */
         
-        // First we create a level that is used bij floor 1.
-        Level floor1 = new Level(1);
+        // First we create a level that is used bij floor 0.
+        Level oldLevel = game.getCurrentLevel();
         
+        game.nextLevel();
+        Assert.assertEquals("nextLevel() did not increase the currentRound with 1", 0, game.getCurrentRound());
+        Assert.assertNotSame("oldLevel is the same level after calling nextLevel() method", oldLevel, game.getCurrentLevel());
         
+        // Let's test if the startRound has been called.
+        Assert.assertEquals("nextLevel() method didn't call startRound() method, because game isn't running",
+                            true, game.getIsRunning());
     }
        
     @Test
@@ -100,6 +107,11 @@ public class GameTest {
     /**
      * starts the next round at the current floor.
      */
+        
+     game.startRound();
+     Assert.assertEquals("startRound() method isn't correct because the isRunnning state is still false", 
+                         true, game.getIsRunning());
+     
     }
     
     @Test
@@ -108,21 +120,56 @@ public class GameTest {
      * ends the current round, after this the next level will be generated 
      * and the current round will increase with one.
      */
+        game.startRound();
+        game.endRound();
+        Assert.assertEquals("endRound() method isn't ccrrect because the isRunning state is still true",
+                            false, game.getIsRunning());
+    }
+    
+    @Test 
+    public void testEndGame(){
+    /**
+     * Will be called when the game is done. 
+     * There might be a victory screen.
+     */
+        try{
+            game.nextLevel(); // currentRound increases to 0. 
+            game.endRound(); // isRunning state will be set to false
+            game.endGame(); // Illegal call because the method was called before the game reached the last floor.
+        } catch(Exception e){
+            Assert.fail(e.getMessage());
+        }        
     }
     
     @Test
-    public void testEndGame(){
-    /**
-     * Will be called when the game is done. There might be a victory screen.
-     */
+    public void testEndGameIsRunning(){
+        /**
+         * isRunning has to be false
+         */
+            int floors2 = 2;
+            Game game2 = new Game(players, floors);
+            game2.nextLevel(); // currentRound increases to 0
+            game2.endRound(); // isRunning state will be set to false
+            game2.nextLevel(); // currentRound increases to 1 (Legal for endGame() method)
+            try{
+                game.endGame(); // called on isRunning = true - which will throw an exception
+            }
+            catch(Exception e){
+                Assert.fail(e.getMessage());
+            }
+           
     }
     
     @Test
     public void testLeaveGame(){
     /**
      * player leaves the game
-     * First iteration doesn't need a parameter because we only have two player.
+     * First iteration doesn't need a parameter because we only have two players.
      */
+        game.leaveGame();
+        Assert.assertEquals("leaveGame() method isn't correct because after calling this methode the isRunning "
+                             + "state should be false", false, game.getIsRunning());
+        
     }
     
     @Test
@@ -131,6 +178,11 @@ public class GameTest {
      * Engine of the game. 
      * Makes al calculations for the game and checks if there are any changes to the game state.
      */
+        
+        // I DON'T KNOW HOW TO TEST THIS. 
+        game.tick();
+        Timer timer = game.getTickTimer();
+        
     }
 
 }
