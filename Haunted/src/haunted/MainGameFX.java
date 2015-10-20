262bd7f54fe692bc94b74e8764d802f07adcfd31
Joris van de Wijgert <jvdwijgert@hotmail.com>
2015-10-20 14:57:02 +0200

    MainGameFX Improved
    only last part: background drawing and key/door drawing

--- Haunted/src/haunted/MainGameFX.java
+++ Haunted/src/haunted/MainGameFX.java
@@ -7,15 +7,18 @@ package haunted;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.util.ArrayList;
 import java.util.List;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 /**
@@ -26,7 +29,11 @@ public class MainGameFX extends Application {
 
     double width;
     double height;
+
+    ArrayList<String> pressedKeys;
     
+    final long startNanoTime = System.nanoTime();
+
     Human human;
     Ghost ghost;
     List<Obstacle> obstacles;
@@ -44,10 +51,32 @@ public class MainGameFX extends Application {
         Canvas canvas = new Canvas(width, height);
         GraphicsContext gc = canvas.getGraphicsContext2D();
         root.getChildren().add(canvas);
+
+        pressedKeys = new ArrayList();
+
+        scene.setOnKeyPressed(
+                new EventHandler<KeyEvent>() {
+                    public void handle(KeyEvent e) {
+                        String code = e.getCode().toString();
+
+                        // only add once... prevent duplicates
+                        if (!pressedKeys.contains(code)) {
+                            pressedKeys.add(code);
+                        }
+                    }
+                });
+        scene.setOnKeyReleased(
+                new EventHandler<KeyEvent>() {
+                    public void handle(KeyEvent e) {
+                        String code = e.getCode().toString();
+                        pressedKeys.remove(code);
+                    }
+                });
+
         new AnimationTimer() {
             public void handle(long currentNanoTime) {
                 gc.clearRect(0, 0, width, height);
-                drawElements(gc);
+                drawElements(gc, currentNanoTime);
             }
         }.start();
         stage.show();
@@ -55,20 +84,45 @@ public class MainGameFX extends Application {
     }
 
     //TODO: handling 3 versions when moving
-    public void drawElements(GraphicsContext gc) {
-        for (String img : human.getSprites()) {
-            Image humanImg = new Image(img);
-            gc.drawImage(humanImg, human.getPosition().getX(), human.getPosition().getY(), width/levelWidth, height/levelHeight);
+    public void drawElements(GraphicsContext gc, long currentNanoTime) {
+        int time = Math.round((currentNanoTime - startNanoTime) / 90000000);
+        if (!pressedKeys.contains("UP") && !pressedKeys.contains("DOWN") && !pressedKeys.contains("LEFT") && !pressedKeys.contains("RIGHT")) {
+            Image humanImg = new Image(human.getSprites()[0]);
+            gc.drawImage(humanImg, human.getPosition().getX(), human.getPosition().getY(), width / levelWidth, height / levelHeight);
+        } else {
+            Image humanImg = null;
+            if (time % 3 == 0) {
+                humanImg = new Image(human.getSprites()[0]);
+            } else if (time % 3 == 1) {
+                humanImg = new Image(human.getSprites()[1]);
+            } else if (time % 3 == 2) {
+                humanImg = new Image(human.getSprites()[2]);
+            }
+            if(humanImg != null){
+                gc.drawImage(humanImg, human.getPosition().getX(), human.getPosition().getY(), width / levelWidth, height / levelHeight);
+            }
         }
 
-        for (String img : ghost.getSprites()) {
-            Image ghostImg = new Image(img);
-            gc.drawImage(ghostImg, ghost.getPosition().getX(), ghost.getPosition().getY(), width/levelWidth, height/levelHeight);
+        if (!pressedKeys.contains("W") && !pressedKeys.contains("A") && !pressedKeys.contains("S") && !pressedKeys.contains("D")) {
+            Image ghostImg = new Image(ghost.getSprites()[0]);
+            gc.drawImage(ghostImg, ghost.getPosition().getX(), ghost.getPosition().getY(), width / levelWidth, height / levelHeight);
+        } else {
+            Image ghostImg = null;
+            if (time % 3 == 0) {
+                ghostImg = new Image(human.getSprites()[0]);
+            } else if (time % 3 == 1) {
+                ghostImg = new Image(human.getSprites()[1]);
+            } else if (time % 3 == 2) {
+                ghostImg = new Image(human.getSprites()[2]);
+            }
+            if(ghostImg != null){
+                gc.drawImage(ghostImg, ghost.getPosition().getX(), ghost.getPosition().getY(), width / levelWidth, height / levelHeight);
+            }
         }
 
         for (Obstacle o : obstacles) {
             Image obstImg = new Image(o.getSprite());
-            gc.drawImage(obstImg, o.getPosition().getX(), o.getPosition().getY(), width/levelWidth, height/levelHeight);
+            gc.drawImage(obstImg, o.getPosition().getX(), o.getPosition().getY(), width / levelWidth, height / levelHeight);
         }
     }
 
@@ -84,6 +138,10 @@ public class MainGameFX extends Application {
         this.levelWidth = lvl.getWidth();
         this.levelHeight = lvl.getHeight();
         this.obstacles = lvl.getObstacles();
+    }
+
+    public ArrayList<String> getPressedKeys() {
+        return pressedKeys;
     }
 
     /**
