8dda0624ec37789bcf880da3b5fbd0514c2479d6
Joris van de Wijgert <jvdwijgert@hotmail.com>
2015-11-18 20:51:46 +0100

    javadoc for human and character
    if there is more to do, please let me know

--- HauntedServer/src/hauntedserver/Character.java
+++ HauntedServer/src/hauntedserver/Character.java
@@ -20,14 +20,16 @@ public abstract class Character {
     private Player controller;
     
     /**
-     * 
+     * Constructor for character
+     * Initialize position, movementSpeed, controller
      */
     public Character(){
         
     }
     
     /**
-     * 
+     * Moves the character into the given direction. 
+     * But be aware of collision with another characters, key, walls or door.
      * @param direction 
      */
     public void move(DirectionType direction){
@@ -35,26 +37,27 @@ public abstract class Character {
     }
     
     /**
-     * 
+     * Checks if there is a collision with another character, walls, key or door.
      */
     public void detectCollision(){
         
     }
     
     /**
-     * 
-     * @return 
+     * Returns a list of hitbox points, the points where the character can be hitted on.
+     * Important for the collisioncheck.
+     * @return list of hitbox points.
      */
     public List<Point2D> getHitBoxPoints(){
         return null;
     }
     
     /**
-     * 
+     * returns true if i is between or equal to min and max
      * @param i
      * @param min
      * @param max
-     * @return 
+     * @return true if between or equal
      */
     public boolean betweenInclusive(int i, int min, int max){
         return false;
--- HauntedServer/src/hauntedserver/Human.java
+++ HauntedServer/src/hauntedserver/Human.java
@@ -16,42 +16,49 @@ public class Human extends Character {
     private double[][] flashlightPoints;
 
     /**
-     *
+     * The Constructor for human.
+     * This initializes the flashlightRange, flashlightAngle and the flashlightPoints by calling setFlashlight
+     * Also initialize the base class Character!
      */
     public Human() {
         
     }
     
     /**
-     * 
+     * if haskey == false, hasKey becomes true
      */
     public void pickUpKey(){
         
     }
     
     /**
-     * 
+     * if haskey == true, enterDoor will cause endround, so the game can continue to the next round or go to the victory screen.
+     * Otherwise, this method won't do anything.
      */
     public void enterDoor(){
         
     }
     
     /**
-     * 
+     * check if the human interacts with ghost, key, door or wall
      */
     public void checkInteract(){
         
     }
     
     /**
-     * 
+     * Initializes the flashlight for the human.
+     * With this, the human can see the things in the level, but only the things in range of the flashlight.
+     * Flashlight is a triangle.
      */
     public void setFlashlight(){
         
     }
     
     /**
-     * 
+     * Returns the coordinates of the flashlight polygon
+     * So the fx part can draw this for the human.
+     * @returns polygon of the flashlight.
      */
     public int[] getFlashlightPolygon(){
         return null;
