ebd361bbba4527637ab8f943708cbd56c6818371
Mike Evers <mike_evers1997@hotmail.com>
2015-11-19 15:30:02 +0100

    specification Message Class

--- HauntedServer/src/hauntedserver/Message.java
+++ HauntedServer/src/hauntedserver/Message.java
@@ -18,15 +18,24 @@ public class Message {
     private Player player;
     
     /**
-     * 
-     * @param text
-     * @param player 
+     * Constructs the message object. 
+     * This object represents a message inside the chatbox.
+     * @param text the text that represents the message. 
+     * text is not null, empty or only containing white spaces.
+     * @param player the player that sends the message.
+     * player is not null.
      */
     public Message(String text, Player player){
         this.text = text;
         this.player = player;
     }
     
+    /**
+     * Creates a string of the message object.
+     * Example --> Mike23Hero : Hoi allemaal, veel succes!
+     * @return the string that represents a chat message. 
+     * Returns always a string that is not null.
+     */
     public String toString(){
         return null;
     }
