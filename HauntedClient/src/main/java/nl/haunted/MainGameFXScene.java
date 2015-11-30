b074d6f982b6fc2b78fd596da8a5bcd54714e9d8
Joris van de Wijgert <jvdwijgert@hotmail.com>
2015-11-30 13:14:26 +0100

    more progress in fx

--- HauntedClient/src/main/java/nl/haunted/MainGameFXScene.java
+++ HauntedClient/src/main/java/nl/haunted/MainGameFXScene.java
@@ -5,10 +5,42 @@
  */
 package nl.haunted;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+
 /**
  *
  * @author jvdwi
  */
 public class MainGameFXScene {
+    private double screenWidth, screenHeight;
+    
+    private Scene scene;
+    private Group root;
+    
+    private Canvas bgLayer;
+    private GraphicsContext bgGc;
+        
+    public Scene MainGameFX(Gamefeed gf){
+        root = new Group();
+        scene = new Scene(root);
+        determineScreenSizes();
+        
+        bgLayer = new Canvas(screenWidth, screenHeight);
+        bgGc = bgLayer.getGraphicsContext2D();
+        root.getChildren().add(bgLayer);
+        
+        return scene;
+    }
+    
+    private void determineScreenSizes(){
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        screenWidth = screenSize.getWidth();
+        screenHeight = screenSize.getHeight();
+    }
     
 }
