c00621ee2b96177bc4f965df4974c0cee958f4cc
Joris van de Wijgert <jvdwijgert@hotmail.com>
2015-12-02 14:17:07 +0100

    .classes updated, viewing images possible hopefully...

--- HauntedClient/src/main/java/nl/haunted/MainGameFXScene.java
+++ HauntedClient/src/main/java/nl/haunted/MainGameFXScene.java
@@ -43,15 +43,34 @@ public class MainGameFXScene {
     private Canvas keyDoorLayer, humanLayer, ghostLayer;
     private GraphicsContext keyDoorGc, humanGc, ghostGc;
 
-    Image bgImage;
-
-    List<ImageProps> imgProps;
+    //Images:
+    //Background map
+    private Image bgImage;
+    //Door imgs
+    private Image doorImage;
+    //Key imgs
+    private Image keyImage;
+    //Wall imgs
+    private Image wallImage;
+    //White player imgs
+    private Image[] humanWhiteImages, ghostWhiteImages;
+    //Black player imgs
+    private Image[] humanBlackImages, ghostBlackImages;
+    //Green player imgs
+    private Image[] humanGreenImages, ghostGreenImages;
+    //Red player imgs
+    private Image[] humanRedImages, ghostRedImages;
+    //Blue player imgs
+    private Image[] humanBlueImages, ghostBlueImages;
+    //Orange player imgs
+    private Image[] humanOrangeImages, ghostOrangeImages;
+    //Purple player imgs
+    private Image[] humanPurpleImages, ghostPurpleImages;
 
 //    Image image;
     private gamefeed gf;
 
     public Scene MainGameFX(gamefeed gf) {
-        imgProps = new ArrayList();
         this.gf = gf;
         state = 0;
         this.bgImage = gf.gameInfo.getBackgroundImage();
@@ -101,52 +120,50 @@ public class MainGameFXScene {
     }
 
     private void loadInImages() {
-        for (Entity e : gf.gameInfo.getEntities()) {
-            switch (e.getType()) {
-                case Door:
-                    Image[] doorSprites = new Image[]{new Image("door.png")};
-                    imgProps.add(new ImageProps(doorSprites, e));
-                    break;
-                case Key:
-                    Image[] keySprites = new Image[]{new Image("key.png")};
-                    imgProps.add(new ImageProps(keySprites, e));
-                    break;
-                case Human:
-                    String humanImgText = "human" + getStringFromColor(e.getColor());
-                    Image[] humanSprites = new Image[]{new Image(humanImgText + "1.png"), new Image(humanImgText + "2.png"), new Image(humanImgText + "3.png")};
-                    imgProps.add(new ImageProps(humanSprites, e));
-                    break;
-                case Ghost:
-                    String ghostImgText = "ghost" + getStringFromColor(e.getColor());
-                    Image[] ghostSprites = new Image[]{new Image(ghostImgText + "1.png"), new Image(ghostImgText + "2.png"), new Image(ghostImgText + "3.png"), new Image("wall.png")};
-                    imgProps.add(new ImageProps(ghostSprites, e));
-                    break;
-            }
-        }
+        keyImage = new Image("key.png");
+        doorImage = new Image("door.png");
+        wallImage = new Image("wall.png");
+        humanWhiteImages = new Image[]{new Image("humanWhite1.png"), new Image("humanWhite2.png"), new Image("humanWhite3.png")};
+        ghostWhiteImages = new Image[]{new Image("ghostWhite1.png"), new Image("ghostWhite2.png"), new Image("ghostWhite3.png")};
+        humanBlackImages = new Image[]{new Image("humanBlack1.png"), new Image("humanBlack2.png"), new Image("humanBlack3.png")};
+        ghostBlackImages = new Image[]{new Image("ghostBlack1.png"), new Image("ghostBlack2.png"), new Image("ghostBlack3.png")};
+        humanGreenImages = new Image[]{new Image("humanGreen1.png"), new Image("humanGreen2.png"), new Image("humanGreen3.png")};
+        ghostGreenImages = new Image[]{new Image("ghostGreen1.png"), new Image("ghostGreen2.png"), new Image("ghostGreen3.png")};
+        humanRedImages = new Image[]{new Image("humanRed1.png"), new Image("humanRed2.png"), new Image("humanRed3.png")};
+        ghostRedImages = new Image[]{new Image("ghostRed1.png"), new Image("ghostRed2.png"), new Image("ghostRed3.png")};
+        humanBlueImages = new Image[]{new Image("humanBlue1.png"), new Image("humanBlue2.png"), new Image("humanBlue3.png")};
+        ghostBlueImages = new Image[]{new Image("ghostBlue1.png"), new Image("ghostBlue2.png"), new Image("ghostBlue3.png")};
+        humanOrangeImages = new Image[]{new Image("humanOrange1.png"), new Image("humanOrange2.png"), new Image("humanOrange3.png")};
+        ghostOrangeImages = new Image[]{new Image("ghostOrange1.png"), new Image("ghostOrange2.png"), new Image("ghostOrange3.png")};
+        humanPurpleImages = new Image[]{new Image("humanPurple1.png"), new Image("humanPurple2.png"), new Image("humanPurple3.png")};
+        ghostPurpleImages = new Image[]{new Image("ghostPurple1.png"), new Image("ghostPurple2.png"), new Image("ghostPurple3.png")};
+
     }
 
     private void drawImages() {
-        for (ImageProps imgp : imgProps) {
-            switch (imgp.getEntity().getType()) {
+        for (Entity e : gf.gameInfo.getEntities()) {
+            switch (e.getType()) {
                 case Door:
-                    drawRotatedImage(keyDoorGc, imgp.getImage(0), getAngle(imgp.getEntity().getDirection()), imgp.getEntity().getPosition().getX(), imgp.getEntity().getPosition().getY(), horScale, verScale);
+                    drawRotatedImage(keyDoorGc, doorImage, getAngle(e.getDirection()), e.getPosition().getX(), e.getPosition().getY(), horScale, verScale);
                     break;
                 case Key:
-                    drawRotatedImage(keyDoorGc, imgp.getImage(0), getAngle(imgp.getEntity().getDirection()), imgp.getEntity().getPosition().getX(), imgp.getEntity().getPosition().getY(), horScale, verScale);
+                    keyDoorGc.drawImage(keyImage, e.getPosition().getX(), e.getPosition().getY(), keyImage.getWidth() * horScale, keyImage.getHeight() * verScale);
                     break;
                 case Human:
                     //todo : draw animated human images
-                    drawRotatedImage(humanGc, getAnimatedImage(imgp.getImages()), getAngle(imgp.getEntity().getDirection()), imgp.getEntity().getPosition().getX(), imgp.getEntity().getPosition().getY(), horScale, verScale);
+                    drawRotatedImage(humanGc, getAnimatedHumanImage(e), getAngle(e.getDirection()), e.getPosition().getX(), e.getPosition().getY(), horScale, verScale);
                     break;
                 case Ghost:
                     //todo : draw animated ghost images
-                    if (imgp.getEntity().getWall()) {
-                        int xPos = (((int) imgp.getEntity().getPosition().getX()) + 50) / 100 * 100;
-                        int yPos = (((int) imgp.getEntity().getPosition().getY()) + 50) / 100 * 100;
-                        drawRotatedImage(ghostGc, imgp.getImage(4), 0, xPos, yPos, horScale, verScale);
+                    if (e.getWall()) {
+                        int xPos = (((int) e.getPosition().getX()) + 50) / 100 * 100;
+                        int yPos = (((int) e.getPosition().getY()) + 50) / 100 * 100;
+                        drawRotatedImage(ghostGc, wallImage, 0, xPos, yPos, horScale, verScale);
+                    } else {
+                        drawRotatedImage(ghostGc, getAnimatedGhostImage(e), getAngle(e.getDirection()), e.getPosition().getX(), e.getPosition().getY(), horScale, verScale);
                     }
-                    drawRotatedImage(ghostGc, getAnimatedImage(imgp.getImages()), getAngle(imgp.getEntity().getDirection()), imgp.getEntity().getPosition().getX(), imgp.getEntity().getPosition().getY(), horScale, verScale);
                     break;
+
             }
         }
     }
@@ -178,43 +195,230 @@ public class MainGameFXScene {
         gc.restore(); // back to original state (before rotation)
     }
 
-    private String getStringFromColor(Color c) {
-        String name = "";
+    private Image getAnimatedHumanImage(Entity e) {
+        Image returnImage = null;
+
+        Color c = e.getColor();
         if (c == Color.WHITE) {
-            return "white";
+            switch (state) {
+                case 2:
+                    returnImage = humanWhiteImages[0];
+                    state = 0;
+                    break;
+                case 0:
+                    returnImage = humanWhiteImages[1];
+                    state = 1;
+                    break;
+                case 1:
+                    returnImage = humanWhiteImages[2];
+                    state = 2;
+                    break;
+            }
         } else if (c == Color.BLACK) {
-            return "black";
+            switch (state) {
+                case 2:
+                    returnImage = humanBlackImages[0];
+                    state = 0;
+                    break;
+                case 0:
+                    returnImage = humanBlackImages[1];
+                    state = 1;
+                    break;
+                case 1:
+                    returnImage = humanBlackImages[2];
+                    state = 2;
+                    break;
+            }
         } else if (c == Color.GREEN) {
-            return "green";
+            switch (state) {
+                case 2:
+                    returnImage = humanGreenImages[0];
+                    state = 0;
+                    break;
+                case 0:
+                    returnImage = humanGreenImages[1];
+                    state = 1;
+                    break;
+                case 1:
+                    returnImage = humanGreenImages[2];
+                    state = 2;
+                    break;
+            }
         } else if (c == Color.RED) {
-            return "red";
+            switch (state) {
+                case 2:
+                    returnImage = humanRedImages[0];
+                    state = 0;
+                    break;
+                case 0:
+                    returnImage = humanRedImages[1];
+                    state = 1;
+                    break;
+                case 1:
+                    returnImage = humanRedImages[2];
+                    state = 2;
+                    break;
+            }
         } else if (c == Color.BLUE) {
-            return "blue";
+            switch (state) {
+                case 2:
+                    returnImage = humanBlueImages[0];
+                    state = 0;
+                    break;
+                case 0:
+                    returnImage = humanBlueImages[1];
+                    state = 1;
+                    break;
+                case 1:
+                    returnImage = humanBlueImages[2];
+                    state = 2;
+                    break;
+            }
         } else if (c == Color.ORANGE) {
-            return "orange";
+            switch (state) {
+                case 2:
+                    returnImage = humanOrangeImages[0];
+                    state = 0;
+                    break;
+                case 0:
+                    returnImage = humanOrangeImages[1];
+                    state = 1;
+                    break;
+                case 1:
+                    returnImage = humanOrangeImages[2];
+                    state = 2;
+                    break;
+            }
         } else if (c == Color.PURPLE) {
-            return "purple";
-        } else {
-            return "trans";
+            switch (state) {
+                case 2:
+                    returnImage = humanPurpleImages[0];
+                    state = 0;
+                    break;
+                case 0:
+                    returnImage = humanPurpleImages[1];
+                    state = 1;
+                    break;
+                case 1:
+                    returnImage = humanPurpleImages[2];
+                    state = 2;
+                    break;
+            }
         }
+
+        return returnImage;
     }
 
-    private Image getAnimatedImage(Image[] sprites) {
+    private Image getAnimatedGhostImage(Entity e) {
         Image returnImage = null;
-        switch (state) {
-            case 2:
-                returnImage = sprites[0];
-                state = 0;
-                break;
-            case 0:
-                returnImage = sprites[1];
-                state = 1;
-                break;
-            case 1:
-                returnImage = sprites[2];
-                state = 2;
-                break;
+        Color c = e.getColor();
+        if (c == Color.WHITE) {
+            switch (state) {
+                case 2:
+                    returnImage = ghostWhiteImages[0];
+                    state = 0;
+                    break;
+                case 0:
+                    returnImage = ghostWhiteImages[1];
+                    state = 1;
+                    break;
+                case 1:
+                    returnImage = ghostWhiteImages[2];
+                    state = 2;
+                    break;
+            }
+        } else if (c == Color.BLACK) {
+            switch (state) {
+                case 2:
+                    returnImage = ghostBlackImages[0];
+                    state = 0;
+                    break;
+                case 0:
+                    returnImage = ghostBlackImages[1];
+                    state = 1;
+                    break;
+                case 1:
+                    returnImage = ghostBlackImages[2];
+                    state = 2;
+                    break;
+            }
+        } else if (c == Color.GREEN) {
+            switch (state) {
+                case 2:
+                    returnImage = ghostGreenImages[0];
+                    state = 0;
+                    break;
+                case 0:
+                    returnImage = ghostGreenImages[1];
+                    state = 1;
+                    break;
+                case 1:
+                    returnImage = ghostGreenImages[2];
+                    state = 2;
+                    break;
+            }
+        } else if (c == Color.RED) {
+            switch (state) {
+                case 2:
+                    returnImage = ghostRedImages[0];
+                    state = 0;
+                    break;
+                case 0:
+                    returnImage = ghostRedImages[1];
+                    state = 1;
+                    break;
+                case 1:
+                    returnImage = ghostRedImages[2];
+                    state = 2;
+                    break;
+            }
+        } else if (c == Color.BLUE) {
+            switch (state) {
+                case 2:
+                    returnImage = ghostBlueImages[0];
+                    state = 0;
+                    break;
+                case 0:
+                    returnImage = ghostBlueImages[1];
+                    state = 1;
+                    break;
+                case 1:
+                    returnImage = ghostBlueImages[2];
+                    state = 2;
+                    break;
+            }
+        } else if (c == Color.ORANGE) {
+            switch (state) {
+                case 2:
+                    returnImage = ghostOrangeImages[0];
+                    state = 0;
+                    break;
+                case 0:
+                    returnImage = ghostOrangeImages[1];
+                    state = 1;
+                    break;
+                case 1:
+                    returnImage = ghostOrangeImages[2];
+                    state = 2;
+                    break;
+            }
+        } else if (c == Color.PURPLE) {
+            switch (state) {
+                case 2:
+                    returnImage = ghostPurpleImages[0];
+                    state = 0;
+                    break;
+                case 0:
+                    returnImage = ghostPurpleImages[1];
+                    state = 1;
+                    break;
+                case 1:
+                    returnImage = ghostPurpleImages[2];
+                    state = 2;
+                    break;
+            }
         }
+
         return returnImage;
     }
 
