ca30413b056dbbce59d5f27c88a30c200f1eff60
cyrilbrugman <cyril.brugman@gmail.com>
2015-12-14 12:27:05 +0100

    FLOPDIEDOODLE

--- HauntedClient/src/main/java/nl/haunted/ClientController.java
+++ HauntedClient/src/main/java/nl/haunted/ClientController.java
@@ -7,8 +7,10 @@ package nl.haunted;
 
 import java.beans.PropertyChangeEvent;
 import java.io.IOException;
-import java.net.Inet4Address;
+import java.net.Inet6Address;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
@@ -16,6 +18,8 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -48,7 +52,7 @@ public class ClientController extends UnicastRemoteObject implements IClientCont
         return gamelobbys;
     }
 
-    public ClientController(HauntedClient client, String ip) throws RemoteException, UnknownHostException {
+    public ClientController(HauntedClient client, String ip) throws RemoteException, UnknownHostException, IOException {
         gamelobbys = new ArrayList();
         this.client = client;
         startClient(ip);
@@ -59,7 +63,7 @@ public class ClientController extends UnicastRemoteObject implements IClientCont
         //player = lobby.createPlayer("player", "get ip adress not implemented yeti");create return type for createplayer 
     }
 
-    public void startClient(String ip) throws RemoteException, UnknownHostException {
+    public void startClient(String ip) throws RemoteException, UnknownHostException, IOException {
         // Locate registry at ip address (server) with port 8761.
         try {
             registry = LocateRegistry.getRegistry(ip, 8761);
@@ -90,9 +94,49 @@ public class ClientController extends UnicastRemoteObject implements IClientCont
                 ex.printStackTrace();
                 System.out.println("Client: Controller couldn't subscribe himself to the lobby.");
             }
-        }      
-        tisplayer = lobby.createPlayer("player",this.inputController.getSrvSocket().getIPAddress());
+        }           
+        tisplayer = lobby.createPlayer("player",this.getIPadress());
     }
+    
+    
+   private String getIPadress() throws SocketException, IOException{
+       Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+        OUTER:
+        for (NetworkInterface interface_ : Collections.list(interfaces)) {
+            // we shouldn't care about loopback addresses
+            if (interface_.isLoopback()) {
+                continue;
+            }
+
+            // if you don't expect the interface to be up you can skip this
+            // though it would question the usability of the rest of the code
+            if (!interface_.isUp()) {
+                continue;
+            }
+
+            // iterate over the addresses associated with the interface
+            Enumeration<InetAddress> addresses = interface_.getInetAddresses();
+            for (InetAddress address : Collections.list(addresses)) {
+                // look only for ipv4 addresses
+                if (address instanceof Inet6Address) {
+                    continue;
+                }
+
+                // use a timeout big enough for your needs
+                if (!address.isReachable(3000)) {
+                    continue;
+                }
+
+                // java 7's try-with-resources statement, so that
+                // we close the socket immediately after use
+                if (address.toString().indexOf("10.1.3.") > 0) {
+                    System.out.println(address.getHostAddress());
+                    return interface_.getInetAddresses().nextElement().getHostAddress();
+                }
+            }
+        }
+        return "";
+   }
 
     /**
      * @param propertyChangeEvent
