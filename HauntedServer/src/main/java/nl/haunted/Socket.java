97eaea96bb53069969ad5266827c32c2805c1cd8
cyrilbrugman <cyril.brugman@gmail.com>
2015-12-14 10:18:51 +0100

    socket

--- HauntedServer/src/main/java/nl/haunted/Socket.java
+++ HauntedServer/src/main/java/nl/haunted/Socket.java
@@ -44,7 +44,7 @@ public class Socket implements Serializable {
 
     public void socketSetup(String groupname, int port) throws IOException {
         sock = new MulticastSocket(port);
-        sock.setLoopbackMode(true);
+        sock.setLoopbackMode(false);
         groupIp = InetAddress.getByName(groupname);
         Scanner input = new Scanner(System.in);
         nic = this.getLocalNIC();//this.getInternetNIC(); //commented for futuure over internet support
