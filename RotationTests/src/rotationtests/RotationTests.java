74a0831f10179ee284031452aa5f15b9f36e8f5d
Joris van de Wijgert <jvdwijgert@hotmail.com>
2016-01-04 11:13:01 +0100

    sync

---
+++ RotationTests/src/rotationtests/RotationTests.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rotationtests;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.util.Timer;
+import java.util.TimerTask;
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.transform.Rotate;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author jvdwi
+ */
+public class RotationTests extends Application {
+
+    private int rotation = 0;
+    private double screenWidth, screenHeight;
+    private double horScale, verScale;
+    Timer timer;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.determineScreenSizes();
+        Canvas canvas = new Canvas(1920, 1080);
+        Canvas canvas2 = new Canvas((102), (102));
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        GraphicsContext gc2 = canvas2.getGraphicsContext2D();
+        StackPane root = new StackPane();
+        root.getChildren().add(canvas);
+        root.getChildren().add(canvas2);
+        Color col = Color.GREEN;
+        Image image = new Image("img.png");
+        Scene scene = new Scene(root, 1920, 1080);
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        AnimationTimer at = new AnimationTimer() {
+            public void handle(long currentNanoTime) {
+                gc.clearRect(0, 0, 1920, 1080);
+                drawRotatedImage(gc, image, rotation, 500, 500, horScale, verScale);
+
+
+            }
+        };
+        at.start();
+        timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+
+                rotation = rotation + 180;
+                if (rotation == 360) {
+                    rotation = 0;
+                }
+            }
+        };
+        timer.scheduleAtFixedRate(task, 0, 1000);
+        primaryStage.show();
+    }
+
+    private void rotate(GraphicsContext gc, double angle, double px, double py) {
+        Rotate r = new Rotate(angle, px, py);
+        gc.setTransform(r.getMxx(), r.getMyx(), r.getMxy(), r.getMyy(), r.getTx(), r.getTy());
+    }
+
+    private void drawRotatedImage(GraphicsContext gc, Image image, double angle, double tlpx, double tlpy, double scaleX, double scaleY) {
+        gc.save(); // saves the current state on stack, including the current transform
+        
+        if (angle == 0) {
+            rotate(gc, angle, (tlpx + (image.getWidth() / 2)), (tlpy + (image.getHeight() / 2)));
+            gc.drawImage(image, tlpx, tlpy, image.getWidth() * scaleX, image.getHeight() * scaleY);
+        } else if (angle == 180){
+            rotate(gc, angle, (tlpx - (image.getWidth() / 2)), (tlpy - (image.getHeight() / 2)));
+            gc.drawImage(image, tlpx, tlpy, image.getWidth() * scaleX, image.getHeight() * scaleY);
+        }
+//        else {
+//            gc.drawImage(image, tlpx, tlpy, image.getWidth() * scaleX, image.getHeight() * scaleY);
+//        }
+        gc.restore(); // back to original state (before rotation)
+    }
+
+
+
+    private String getStringFromColor(Color c) {
+        String name = "";
+        if (c == Color.RED) {
+            return "red";
+        } else if (c == Color.GREEN) {
+            return "Oploo";
+        } else if (c == Color.BLUE) {
+            return "blue";
+        } else {
+            return "red";
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private void determineScreenSizes() {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        screenWidth = screenSize.getWidth();
+        screenHeight = screenSize.getHeight();
+        horScale = (screenWidth / 1700) - ((screenWidth / 1700) % 0.1);
+        verScale = (screenHeight / 1200) - ((screenWidth / 1700) % 0.1);
+        double temphorscale = (1366.0 / 1700) - ((1366.0 / 1700) % 0.1);
+        double tempverscale = (768.0 / 1200) - ((768.0 / 1700) % 0.1);
+    }
+
+}
