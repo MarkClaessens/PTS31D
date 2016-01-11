a8a37847e46c29d70d9db222d130c4ef0543c5f9
Joris van de Wijgert <jvdwijgert@hotmail.com>
2016-01-11 14:21:02 +0100

    comment

--- HauntedClient/src/main/java/nl/haunted/MainGameFXScene.java
+++ HauntedClient/src/main/java/nl/haunted/MainGameFXScene.java
@@ -178,7 +178,7 @@ public class MainGameFXScene {
         AnimationTimer at = new AnimationTimer() {
             @Override
             public void handle(long currentNanoTime) {
-                if (!gf.gameInfo.isGameEnd() || !gf.gameInfo.isRoundEnd()) { //TODO
+                if (!gf.gameInfo.isGameEnd() || !gf.gameInfo.isRoundEnd()) {
 
                     keyDoorGc.clearRect(0, 0, screenWidth, screenHeight);
                     humanGc.clearRect(0, 0, screenWidth, screenHeight);
