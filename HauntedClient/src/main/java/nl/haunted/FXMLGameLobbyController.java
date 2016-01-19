fbb6042296cf81740ce340de062f2595e4ca4f8e
MarkClaessens <mark-claessens@hotmail.com>
2016-01-19 12:42:29 +0100

    bugfix

--- HauntedClient/src/main/java/nl/haunted/FXMLGameLobbyController.java
+++ HauntedClient/src/main/java/nl/haunted/FXMLGameLobbyController.java
@@ -135,14 +135,14 @@ public class FXMLGameLobbyController extends UnicastRemoteObject implements Init
 
     private void playernames() throws RemoteException {
         List<String> namen = new ArrayList<>();
-        if (!gamelobby.getHost().equals(tisplayer)) {
-                IVstart.setVisible(false);
-                TFnewplayers.setVisible(false);
-                TFnewfloors.setVisible(false);
-                TFnewname.setVisible(false);
-                BTNsetname.setVisible(false);
-                BTNsetfloors.setVisible(false);
-                BTNsetplayers.setVisible(false);
+        if (gamelobby.getHost().equals(tisplayer)) {
+                IVstart.setVisible(true);
+                TFnewplayers.setVisible(true);
+                TFnewfloors.setVisible(true);
+                TFnewname.setVisible(true);
+                BTNsetname.setVisible(true);
+                BTNsetfloors.setVisible(true);
+                BTNsetplayers.setVisible(true);
             }
         for (IPlayer player : players) {
             if (gamelobby.getHost().equals(player)) {
