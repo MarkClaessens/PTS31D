25aaeef3ebd7165806f06e7770dbd4213aa7ef1d
Joris van de Wijgert <jvdwijgert@hotmail.com>
2016-01-19 13:34:33 +0100

    small change

--- HauntedClient/src/main/java/nl/haunted/MainGameFXScene.java
+++ HauntedClient/src/main/java/nl/haunted/MainGameFXScene.java
@@ -567,7 +567,7 @@ public class MainGameFXScene {
                         Image ghostImgToDraw = getAnimatedGhostImage(e, ghostChooser);
                         ghostGcs[ghostChooser].drawImage(ghostImgToDraw, (e.getPosition().getX() + 100) * horScale, (e.getPosition().getY() + 100) * verScale, ghostImgToDraw.getWidth() * horScale, ghostImgToDraw.getHeight() * verScale);
                     } else if (e.getRip()) {
-                        ghostGcs[ghostChooser].drawImage(deadImage, (e.getPosition().getX() + 100) * horScale, (e.getPosition().getY() + 100) * verScale, wallImage.getWidth() * horScale, wallImage.getHeight() * verScale);
+                        ghostGcs[ghostChooser].drawImage(deadImage, (e.getPosition().getX() + 100) * horScale, (e.getPosition().getY() + 100) * verScale, deadImage.getWidth() * horScale, deadImage.getHeight() * verScale);
                     }
                     ghostChooser++;
                     break;
