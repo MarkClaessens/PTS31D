1e22dfe60693933f2c6588b35beb832abfbc1ff9
Cyril Brugman <cyril.brugman@gmail.com>
2016-01-26 15:40:37 +0100

    server changes

--- HauntedServer/src/main/java/nl/haunted/HauntedServer.java
+++ HauntedServer/src/main/java/nl/haunted/HauntedServer.java
@@ -45,7 +45,7 @@ public class HauntedServer {
 
         // Bind lobby using registry
         try {
-            System.setProperty("java.rmi.server.hostname", "10.1.3.2");
+            System.setProperty("java.rmi.server.hostname", "10.1.3.5");
             registry.rebind(bindingNameLobby, lobby);
         } catch (RemoteException ex) {
             System.out.println("Server: RemoteException: " + ex.getMessage());
@@ -57,7 +57,7 @@ public class HauntedServer {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.setProperty("java.rmi.server.hostname", "10.1.3.2");
+        System.setProperty("java.rmi.server.hostname", "10.1.3.5");
         try {
             // Create server
             RMISocketFactory.setSocketFactory(new MyServerRMISocketFactory());
